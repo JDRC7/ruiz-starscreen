67aecc274f42ddd4a52e804ed17abc8b5e463472
jdrc7 <jdruiz.c@outlook.es>
2026-06-04 02:34:34 -0500

    CREADOS LOS src

---
+++ readme.md
@@ -0,0 +1,11 @@
+nest generate module generos
+nest generate controller generos
+nest generate service generos
+
+nest generate module peliculas
+nest generate controller peliculas
+nest generate service peliculas
+
+nest generate module services
+nest generate controller services
+nest generate service services
---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { GenerosModule } from './generos/generos.module';
+import { PeliculasModule } from './peliculas/peliculas.module';
+import { ServicesModule } from './services/services.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '5432', 10),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    GenerosModule,
+    PeliculasModule,
+    ServicesModule,
+  ],
+})
+export class AppModule {}
